1a981b74b264234e39c594d2c3e373dc29bc87d1
Sancho Montana <SanchoMontana@mail.com>
2018-06-07 21:42:12 -0400

    Allowed the spaceship to rotate when the left and right arrow keys are pressed, and changed its shape from a triangle to an arrow.

--- asteroids.py
+++ asteroids.py
@@ -14,7 +14,7 @@ pygame.display.update()
 
 center = [displayWidth / 2, displayHeight / 2]
 
-def drawRocket(center, direction, acceleration):
+def drawRocket(center, theta, acceleration):
 #    pygame.draw.polygon(gameDisplay,
 #            white,
 #            [[center[0], center[1] - 15], 
@@ -23,34 +23,34 @@ def drawRocket(center, direction, acceleration):
 #            1)
     pygame.draw.polygon(gameDisplay,
             white,
-            [[center[0] + 15 * math.cos(math.radians(direction)), center[1] - 15 * math.sin(math.radians(direction))],
-                [center[0] - 10 * math.cos(math.radians(direction)), center[1] + 15 * math.sin(math.radians(direction))],
-                [center[0] + 10 * math.cos(math.radians(direction)), center[1] + 15 * math.sin(math.radians(direction))]],
-            1)
+            [[center[0] + 20 * math.cos(math.radians(theta)), center[1] - 20 * math.sin(math.radians(theta))],
+                [center[0] + 20 * math.cos(math.radians(theta - 130)), center[1] - 20 * math.sin(math.radians(theta - 130))],
+                [center[0] + 5 * math.cos(math.radians(theta - 180)), center[1] - 5 * math.sin(math.radians(theta - 180))],
+                [center[0] + 20 * math.cos(math.radians(theta + 130)), center[1] - 20 * math.sin(math.radians(theta  + 130))]],
+            2)
     
     
 
 game_exit = False
-direction = 90
-dir_change = 0
+theta = 90
+delta_theta = 0
 while not game_exit:
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_exit = True
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                dir_change = -1
-            elif event.key == pygame.K_d:
-                dir_change = 1
+            if event.key == pygame.K_LEFT:
+                delta_theta += 0.5
+            elif event.key == pygame.K_RIGHT:
+                delta_theta -= 0.5
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                dir_change += 1
-            elif event.key == pygame.K_d:
-                dir_change -= 1
-
-    direction = (direction + dir_change) % 360
+            if event.key == pygame.K_LEFT:
+                delta_theta -= 0.5
+            elif event.key == pygame.K_RIGHT:
+                delta_theta += 0.5
+    theta = (theta + delta_theta) % 360
     gameDisplay.fill(gray)
-    drawRocket(center, direction, 3)
+    drawRocket(center, theta, 3)
     pygame.display.update()
 quit()
