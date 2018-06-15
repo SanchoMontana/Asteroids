73ed6cceb3a483dd6e9acf582e65189db052a8cb
Sancho Montana <spencersnider9000@gmail.com>
2018-06-14 22:13:59 -0400

    Created a split function for the asteroids, it is very buggy. The split function currently is called whenever the spaceship shoots (spacebar) because there is currently no asteroid collision detection.

--- asteroids.py
+++ asteroids.py
@@ -135,34 +135,41 @@ class Shot:
 
 
 class Asteroids:
-    def __init__(self, size, theta, momentum):
+    def __init__(self, size, theta, momentum, center=[]):
         self.size = size
-        self.theta = theta
-
-        # Ensures that the starting point of each asteroid is off of the screen,
-        # and travels across the screen (for the most part).
-        if 0 <= self.theta <= 90:
-            if random.getrandbits(1):
-                self.center = [-50, random.randint(0, DISPLAY_HEIGHT / 2)]
-            else:
-                self.center = [random.randint(0, DISPLAY_WIDTH / 2), -50]
-        elif 90 <= self.theta <= 180:
-            if random.getrandbits(1):
-                self.center = [DISPLAY_WIDTH + 50, random.randint(0, DISPLAY_HEIGHT / 2)]
-            else:
-                self.center = [random.randint(DISPLAY_WIDTH / 2, DISPLAY_WIDTH), -50]
-        elif 180 <= self.theta <= 270:
-            if random.getrandbits(1):
-                self.center = [DISPLAY_WIDTH + 50, random.randint(DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT)]
-            else:
-                self.center = [random.randint(DISPLAY_WIDTH / 2, DISPLAY_WIDTH), DISPLAY_HEIGHT + 50]
-        elif 270 <= self.theta <= 360:
-            if random.getrandbits(1):
-                self.center = [-50, random.randint(DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT)]
-            else:
-                self.center = [random.randint(0, DISPLAY_WIDTH / 2), DISPLAY_HEIGHT + 50]
+        self.new_size = self.size - 1
         self.momentum = momentum
+        self.theta = theta
         self.points = []
+        self.delta_theta = 0
+        self.delta_theta2 = 0
+        self.new_momentum = 0
+        self.new_momentum2 = 0
+        if center:
+            self.center = center
+        else:
+            # Ensures that the starting point of each asteroid is off of the screen,
+            # and travels across the screen (for the most part).
+            if 0 <= self.theta <= 90:
+                if random.getrandbits(1):
+                    self.center = [-50, random.randint(0, DISPLAY_HEIGHT / 2)]
+                else:
+                    self.center = [random.randint(0, DISPLAY_WIDTH / 2), -50]
+            elif 90 <= self.theta <= 180:
+                if random.getrandbits(1):
+                    self.center = [DISPLAY_WIDTH + 50, random.randint(0, DISPLAY_HEIGHT / 2)]
+                else:
+                    self.center = [random.randint(DISPLAY_WIDTH / 2, DISPLAY_WIDTH), -50]
+            elif 180 <= self.theta <= 270:
+                if random.getrandbits(1):
+                    self.center = [DISPLAY_WIDTH + 50, random.randint(DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT)]
+                else:
+                    self.center = [random.randint(DISPLAY_WIDTH / 2, DISPLAY_WIDTH), DISPLAY_HEIGHT + 50]
+            elif 270 <= self.theta <= 360:
+                if random.getrandbits(1):
+                    self.center = [-50, random.randint(DISPLAY_HEIGHT / 2, DISPLAY_HEIGHT)]
+                else:
+                    self.center = [random.randint(0, DISPLAY_WIDTH / 2), DISPLAY_HEIGHT + 50]
 
         # Randomizes the shape of each asteroid
         for point in range(10):
@@ -186,6 +193,20 @@ class Asteroids:
             point[0] += self.momentum * math.cos(math.radians(self.theta))
             point[1] += self.momentum * math.sin(math.radians(self.theta))
         pygame.draw.polygon(gameDisplay, WHITE, self.points, 2)
+
+    def split(self):
+        if self.size > 1:
+            self.delta_theta = random.randint(0, 90)
+            self.delta_theta2 = -1 * self.delta_theta + random.randint(-15, 15)
+            self.new_momentum = random.randint(2, 8)
+            self.new_momentum2 = random.randint(4, 6)
+            asteroids.append(Asteroids(self.new_size, self.theta + self.delta_theta, self.new_momentum, self.center))
+            asteroids.append(Asteroids(self.new_size, self.theta + self.delta_theta2, self.new_momentum2, self.center))
+            asteroids.remove(self)
+            del self
+        else:
+            asteroids.remove(self)
+            del self
 
 
 rocket = Rocket([DISPLAY_WIDTH / 2, DISPLAY_HEIGHT / 2], 90, [0, 0])  # Instantiates a Rocket object.
@@ -217,6 +238,8 @@ while not game_exit:
                 rocket.delta_theta -= 6
             elif event.key == pygame.K_SPACE:
                 shots.append(Shot(rocket.center, rocket.theta))
+                for asteroid in asteroids:
+                    asteroid.split()
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 rocket.thrusters = False
@@ -228,8 +251,8 @@ while not game_exit:
     # gameDisplay updates.
     gameDisplay.fill(GRAY)
 
-    if int(time.time()) % 3 == 0 and int(time.time()) != no_repeat:
-        asteroids.append(Asteroids(random.randint(1, 3), random.randint(1, 360), random.randint(1, 8)))
+    if int(time.time()) % 1 == 0 and int(time.time()) != no_repeat:
+        asteroids.append(Asteroids(random.randint(1, 3), random.randint(1, 360), random.randint(2, 8)))
         no_repeat = int(time.time())
 
     for i in stars:
